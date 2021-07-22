fd8c735a3f57b8b92d49a1391bb63dcdd703013c
jhs512 <jangka512@gmail.com>
2021-07-22 13:14:18 +0900

    세팅

---
+++ src/main/java/com/sbs/exam/exam1/UsrHomeMainServlet.java
@@ -0,0 +1,25 @@
+package com.sbs.exam.exam1;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/usr/home/main")
+public class UsrHomeMainServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		response.getWriter().append("안녕하세요.");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
